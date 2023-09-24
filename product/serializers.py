7961f9a7d66b033193002efd767560ec9beecc2b
ddrag23 <kurodyami@gmail.com>
2023-09-24 17:30:30 +0700

    :bug: fix: choice field serializer

--- product/serializers.py
+++ product/serializers.py
@@ -4,9 +4,9 @@ from .models import Product, Satuan, Status
 
 class ProductSerializer(serializers.ModelSerializer):
     kategori_id = serializers.ChoiceField(
-        choices=[(item.id_satuan, item.nama_satuan) for item in Satuan.objects.all()])
+        choices=[(item.id_satuan, item.nama_satuan) for item in Satuan.objects.all()] if Satuan.objects.exists() else [])
     status_id = serializers.ChoiceField(
-        choices=[(item.id_status, item.nama_status) for item in Status.objects.all()])
+        choices=[(item.id_status, item.nama_status) for item in Status.objects.all()] if Satuan.objects.exists() else [])
     kategori = serializers.CharField(
         source='kategori.nama_satuan', read_only=True)
     status = serializers.CharField(source="status.nama_status", read_only=True)
