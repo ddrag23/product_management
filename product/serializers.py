680d2e181cfa2d29a226ee7924cebf1f5c1c7aac
ddrag23 <kurodyami@gmail.com>
2023-09-24 18:21:10 +0700

    :bug: fix: error  serializer when running initial migration

--- product/serializers.py
+++ product/serializers.py
@@ -3,13 +3,20 @@ from .models import Product, Satuan, Status
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    kategori_id = serializers.ChoiceField(
-        choices=[(item.id_satuan, item.nama_satuan) for item in Satuan.objects.all()] if Satuan.objects.exists() else [])
-    status_id = serializers.ChoiceField(
-        choices=[(item.id_status, item.nama_status) for item in Status.objects.all()] if Satuan.objects.exists() else [])
+    kategori_id = serializers.ChoiceField(choices=[])
+    status_id = serializers.ChoiceField(choices=[])
     kategori = serializers.CharField(
         source='kategori.nama_satuan', read_only=True)
     status = serializers.CharField(source="status.nama_status", read_only=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Fetch choices from the database and populate the ChoiceField choices
+        self.fields['kategori_id'].choices = [
+            (item.id_satuan, item.nama_satuan) for item in Satuan.objects.all()]
+        self.fields['status_id'].choices = [
+            (item.id_status, item.nama_status) for item in Status.objects.all()]
 
     class Meta:
         model = Product
